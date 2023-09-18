dab5679423df8d1d0c1bca010e3872bb90571cb2
Lolu Bolarinwa <lolubolarinwa@gmail.com>
2023-09-18 23:59:48 +0100

    useMemo memoization for theme settings

--- src/theme.jsx
+++ src/theme.jsx
@@ -209,5 +209,5 @@ export const useMode = () => {
     []
   );
 
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+//   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
 };
