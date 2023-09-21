1c9cfa73c99177ce578ddebd21d1640dbe695c3e
Lolu Bolarinwa <lolubolarinwa@gmail.com>
2023-09-21 23:08:05 +0100

    app.jsx init

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import { ColorModeContext, useMode,  } from './theme'
+// import { useState } from 'react'
+import { ColorModeContext, useMode } from "./theme";
+import { ThemeProvider, CssBaseline } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, colorMode] = useMode();
+  // const [count, setCount] = useState(0);
 
   return (
-    <>
-    <div className="app"></div>
-    </>
-  )
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme= {theme} >
+      <>
+        <div className="app"></div>
+      </>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
-export default App
+export default App;
