020ad09d522ad528f0038054986211da7810ad69
Lolu Bolarinwa <lolubolarinwa@gmail.com>
2023-09-29 10:38:03 +0100

    addedsearch button to topbar.jsx

--- src/scenes/global/topbar.jsx
+++ src/scenes/global/topbar.jsx
@@ -1,4 +1,4 @@
-import { Box, IconButton, useTheme } from "@mui/material";
+import { Box, Icon, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext, tokens } from "../../theme";
 import InputBase from "@mui/material";
@@ -9,13 +9,23 @@ import { SettingsOutlinedIcon } from "@mui/icons-material/SettingsOutlined";
 import { PersonOutlinedIcon } from "@mui/icons-material/PersonOutlined";
 import { SearchIcon } from "@mui/icons-material/Search";
 
-const Topbar =()=>{
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
-    const colorMode = useContext(ColorModeContext);
-    return <Box display="flex" justifyContent="">
-        
-        {/* </Box> */}
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* SEARCH BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search"/>
+      </Box>
+      <IconButton sx={{ display: "flex" }}></IconButton>
+    </Box>
+  );
 };
 
 export default Topbar;
